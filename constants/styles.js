4ea486770dc933d06137e876d373f7a12be787c8
jahanzaibmughal <150380133+jahanzaibmughal@users.noreply.github.com>
2023-12-31 22:58:04 +0500

    Add files via upload

---
+++ constants/styles.js
@@ -0,0 +1,17 @@
+export const GlobalStyles = {
+  colors: {
+    theme: "#7f8f32",      // Theme Color
+    primary50: "#f5f7e1",   // Light Greenish Yellow
+    primary100: "#e1ebbf",  // Pale Olive
+    primary200: "#c8d69d",  // Light Moss Green
+    primary400: "#a6b673",  // Olive Drab
+    primary500: "#8f9c5e",  // Olive Green
+    primary700: "#646e3a",  // Dark Olive Green
+    primary800: "#515b2f",  // Deep Olive
+    accent500: "#d2bc2a",   // Mustard
+    error50: "#f7e4e1",    // Light Rose
+    error500: "#c75367",   // Rose
+    gray500: "#b0a193",    // Warm Gray
+    gray700: "#66584c",    // Dark Warm Gray
+  },
+};
